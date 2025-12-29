8a05c641d5363e512d6c074f3e63103ac7ebbd8f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:02 +0800

    Sync selenium-server.rb from core@4cefec9
    RelPath: Formula/s/selenium-server.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/selenium-server.rb
@@ -0,0 +1,61 @@
+class SeleniumServer < Formula
+  desc "Browser automation for testing purposes"
+  homepage "https://www.selenium.dev/"
+  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.34.0/selenium-server-4.34.0.jar"
+  sha256 "c4ca7b2453abec28ac705cfc61321ba7ed62e886fbfd54c997b133d73c69c508"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://www.selenium.dev/downloads/"
+    regex(/href=.*?selenium-server[._-]v?(\d+(?:\.\d+)+)\.jar/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "798758819ac6c139c14969dc21d151c451daac6eb06d765ca8f7c72267ab117a"
+  end
+
+  # depends_on "openjdk"
+
+  on_linux do
+    depends_on arch: :x86_64 # org.openqa.selenium.grid.config.ConfigException: No drivers for arm64 Linux
+  end
+
+  def install
+    libexec.install "selenium-server-#{version}.jar"
+    bin.write_jar_script libexec/"selenium-server-#{version}.jar", "selenium-server"
+  end
+
+  service do
+    run [opt_bin/"selenium-server", "standalone", "--port", "4444"]
+    keep_alive false
+    log_path var/"log/selenium-output.log"
+    error_log_path var/"log/selenium-error.log"
+  end
+
+  test do
+    port = free_port
+    spawn "#{bin}/selenium-server standalone --selenium-manager true --port #{port}"
+
+    parsed_output = nil
+
+    max_attempts = 100
+    attempt = 0
+
+    loop do
+      attempt += 1
+      break if attempt > max_attempts
+
+      sleep 3
+
+      output = Utils.popen_read("curl", "--silent", "localhost:#{port}/status")
+      next unless $CHILD_STATUS.exitstatus.zero?
+
+      parsed_output = JSON.parse(output)
+      break if parsed_output["value"]["ready"]
+    end
+
+    refute_nil parsed_output
+    assert parsed_output["value"]["ready"]
+    assert_match version.to_s, parsed_output["value"]["nodes"].first["version"]
+  end
+end
