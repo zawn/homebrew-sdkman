ea6949f3408229e08589cd630b8cf102708bcc2f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:05 +0800

    Sync mill.rb from core@4cefec9
    RelPath: Formula/m/mill.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/m/mill.rb
@@ -0,0 +1,36 @@
+class Mill < Formula
+  desc "Fast, scalable JVM build tool"
+  homepage "https://mill-build.org/"
+  url "https://search.maven.org/remotecontent?filepath=com/lihaoyi/mill-dist/0.12.11/mill-dist-0.12.11.jar"
+  sha256 "567a44bee9006ac943f8cbec18c38637c544144726cdc699aa4f3584ed52ae93"
+  license "MIT"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=com/lihaoyi/mill-dist/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "317801b292e4332605d7dc2a06b20206cb33db9def24b1af80e451c5cef5afce"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"].shift => "mill"
+    chmod 0555, libexec/"mill"
+    (bin/"mill").write_env_script libexec/"mill", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"build.sc").write <<~SCALA
+      import mill._
+      import mill.scalalib._
+      object foo extends ScalaModule {
+        def scalaVersion = "2.13.11"
+      }
+    SCALA
+    output = shell_output("#{bin}/mill resolve __.compile")
+    assert_equal "foo.compile", output.lines.last.chomp
+  end
+end
