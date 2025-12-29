fdbe3b78d2cd5a881d8362ff0146a50a9ba2f826
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:02 +0800

    Sync scala@3.3.rb from core@4cefec9
    RelPath: Formula/s/scala@3.3.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/scala@3.3.rb
@@ -0,0 +1,53 @@
+class ScalaAT33 < Formula
+  desc "JVM-based programming language"
+  homepage "https://www.scala-lang.org/"
+  url "https://github.com/scala/scala3/releases/download/3.3.6/scala3-3.3.6.tar.gz"
+  sha256 "7267524240ee289976fed1b8bc08c0045d5d290d3faee07c6b7137a4252b5b97"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://www.scala-lang.org/download/"
+    regex(%r{href=.*?download/v?(3\.3(?:\.\d+)+)\.html}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c6ef17066454d96317b625b45b1dd2c61423f9a9f598469e157261f77e3f6c26"
+  end
+
+  keg_only :versioned_formula
+
+  # depends_on "openjdk"
+
+  def install
+    rm Dir["bin/*.bat"]
+    libexec.install "lib"
+    prefix.install "bin"
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+
+    # Set up an IntelliJ compatible symlink farm in 'idea'
+    idea = prefix/"idea"
+    idea.install_symlink libexec/"lib"
+  end
+
+  def caveats
+    <<~EOS
+      To use with IntelliJ, set the Scala home to:
+        #{opt_prefix}/idea
+    EOS
+  end
+
+  test do
+    file = testpath/"Test.scala"
+    file.write <<~SCALA
+      object Test {
+        def main(args: Array[String]): Unit = {
+          println(s"${2 + 2}")
+        }
+      }
+    SCALA
+
+    out = shell_output("#{bin}/scala #{file}").strip
+
+    assert_equal "4", out
+  end
+end
