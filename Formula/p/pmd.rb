8b9a541261dfe298ab4bf1bf918333fbe300f35e
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync pmd.rb from core@4cefec9
    RelPath: Formula/p/pmd.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/p/pmd.rb
@@ -0,0 +1,41 @@
+class Pmd < Formula
+  desc "Source code analyzer for Java, JavaScript, and more"
+  homepage "https://pmd.github.io"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.16.0/pmd-dist-7.16.0-bin.zip"
+  sha256 "8f9fed9eaac4fb1fb9882f8820570dcb1d8647ada158437ba28424a07a48edfd"
+  license "BSD-4-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "16e767469b588a8bd492fa795e81f93559a03b486ede5ed4406a69b53b9b583e"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm Dir["bin/*.bat"]
+    libexec.install Dir["*"]
+    (bin/"pmd").write_env_script libexec/"bin/pmd", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"java/testClass.java").write <<~JAVA
+      public class BrewTestClass {
+        // dummy constant
+        public String SOME_CONST = "foo";
+
+        public boolean doTest () {
+          return true;
+        }
+      }
+    JAVA
+
+    output = shell_output("#{bin}/pmd check -d #{testpath}/java " \
+                          "-R category/java/bestpractices.xml -f json")
+    assert_empty JSON.parse(output)["processingErrors"]
+  end
+end
