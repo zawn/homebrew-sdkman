58eb26defbffd39d8ac2c30047276dd496b01743
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:06 +0800

    Sync mx.rb from core@4cefec9
    RelPath: Formula/m/mx.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/m/mx.rb
@@ -0,0 +1,50 @@
+class Mx < Formula
+  desc "Command-line tool used for the development of Graal projects"
+  homepage "https://github.com/graalvm/mx"
+  url "https://github.com/graalvm/mx/archive/refs/tags/7.60.0.tar.gz"
+  sha256 "f4f8be1b49dd786e20ea7cb4c114e5fbda8b9ac9ab05692432d1b10932ea5fe9"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7884da97bbdc8a7e160d17eb4a374894a6a0845ff57b446fad20e91a78e562fa"
+  end
+
+  # depends_on "openjdk" => :test
+  depends_on "python@3.13"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"mx").write_env_script libexec/"mx", MX_PYTHON: "#{Formula["python@3.13"].opt_libexec}/bin/python"
+    bash_completion.install libexec/"bash_completion/mx" => "mx"
+  end
+
+  def post_install
+    # Run a simple `mx` command to create required empty directories inside libexec
+    Dir.mktmpdir do |tmpdir|
+      with_env(HOME: tmpdir) do
+        system bin/"mx", "--user-home", tmpdir, "version"
+      end
+    end
+  end
+
+  test do
+    resource "homebrew-testdata" do
+      url "https://github.com/oracle/graal/archive/refs/tags/vm-22.3.2.tar.gz"
+      sha256 "77c7801038f0568b3c2ef65924546ae849bd3bf2175e2d248c35ba27fd9d4967"
+    end
+
+    ENV["JAVA_HOME"] = Language::Java.java_home
+    ENV["MX_ALT_OUTPUT_ROOT"] = testpath
+
+    testpath.install resource("homebrew-testdata")
+    cd "vm" do
+      output = shell_output("#{bin}/mx suites")
+      assert_match "distributions:", output
+    end
+  end
+end
