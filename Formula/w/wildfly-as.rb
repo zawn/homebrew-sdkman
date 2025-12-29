8ac215957d155beb6f811367f2cdf9f83d8a1238
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync wildfly-as.rb from core@4cefec9
    RelPath: Formula/w/wildfly-as.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/w/wildfly-as.rb
@@ -0,0 +1,90 @@
+class WildflyAs < Formula
+  desc "Managed application runtime for building applications"
+  homepage "https://www.wildfly.org/"
+  url "https://github.com/wildfly/wildfly/releases/download/36.0.1.Final/wildfly-36.0.1.Final.tar.gz"
+  sha256 "1a0f71680cac962cef03173e81f5ff8886175f18292db158b75c8077cf4ac38d"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://www.wildfly.org/downloads/"
+    regex(/href=.*?wildfly[._-]v?(\d+(?:\.\d+)+)\.Final\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "3227b34a4c0c8a1f4e9fdc304cda6a6858a0e2dc8d00313dc12b66f783888eee"
+    sha256 cellar: :any, arm64_sonoma:  "3227b34a4c0c8a1f4e9fdc304cda6a6858a0e2dc8d00313dc12b66f783888eee"
+    sha256 cellar: :any, arm64_ventura: "3227b34a4c0c8a1f4e9fdc304cda6a6858a0e2dc8d00313dc12b66f783888eee"
+    sha256 cellar: :any, sonoma:        "4cf412f1900615e8dfdfc370fde2022397abcf9cdf517ef13089f3369de4ef45"
+    sha256 cellar: :any, ventura:       "4cf412f1900615e8dfdfc370fde2022397abcf9cdf517ef13089f3369de4ef45"
+  end
+
+  # Installs a pre-built `libartemis-native-64.so` file with linkage to libaio.so.1
+  depends_on :macos
+  # depends_on "openjdk"
+
+  def install
+    buildpath.glob("bin/*.{bat,ps1}").map(&:unlink)
+    rm_r buildpath.glob("**/win-x86_64")
+    rm_r buildpath.glob("**/linux-i686")
+    rm_r buildpath.glob("**/linux-s390x")
+    rm_r buildpath.glob("**/linux-x86_64")
+    rm_r buildpath.glob("**/netty-transport-native-epoll/**/native")
+    if Hardware::CPU.intel?
+      buildpath.glob("**/*_aarch_64.jnilib").map(&:unlink)
+    else
+      rm_r buildpath.glob("**/macosx-x86_64")
+      buildpath.glob("**/*_x86_64.jnilib").map(&:unlink)
+    end
+
+    inreplace "bin/standalone.sh", /JAVA="[^"]*"/, "JAVA='#{Formula["openjdk"].opt_bin}/java'"
+
+    libexec.install Dir["*"]
+    mkdir_p libexec/"standalone/log"
+  end
+
+  def caveats
+    <<~EOS
+      The home of WildFly Application Server #{version} is:
+        #{opt_libexec}
+      You may want to add the following to your .bash_profile:
+        export JBOSS_HOME=#{opt_libexec}
+        export PATH=${PATH}:${JBOSS_HOME}/bin
+    EOS
+  end
+
+  service do
+    run [opt_libexec/"bin/standalone.sh", "--server-config=standalone.xml"]
+    environment_variables JBOSS_HOME: opt_libexec, WILDFLY_HOME: opt_libexec
+    keep_alive successful_exit: false, crashed: true
+  end
+
+  test do
+    ENV["JBOSS_HOME"] = opt_libexec
+    ENV["JBOSS_LOG_DIR"] = testpath
+
+    port = free_port
+
+    pidfile = testpath/"pidfile"
+    ENV["LAUNCH_JBOSS_IN_BACKGROUND"] = "true"
+    ENV["JBOSS_PIDFILE"] = pidfile
+
+    mkdir testpath/"standalone"
+    mkdir testpath/"standalone/deployments"
+    cp_r libexec/"standalone/configuration", testpath/"standalone"
+    fork do
+      exec opt_libexec/"bin/standalone.sh", "--server-config=standalone.xml",
+                                            "-Djboss.http.port=#{port}",
+                                            "-Djboss.server.base.dir=#{testpath}/standalone"
+    end
+    sleep 10
+    sleep 10 if Hardware::CPU.intel?
+
+    begin
+      system "curl", "-X", "GET", "localhost:#{port}/"
+      output = shell_output("curl -s -X GET localhost:#{port}")
+      assert_match "Welcome to WildFly", output
+    ensure
+      Process.kill "SIGTERM", pidfile.read.to_i
+    end
+  end
+end
