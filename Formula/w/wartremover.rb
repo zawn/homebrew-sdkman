8865bc0ea77bfe2a2b7368a201df06c5cc97f576
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync wartremover.rb from core@4cefec9
    RelPath: Formula/w/wartremover.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/w/wartremover.rb
@@ -0,0 +1,39 @@
+class Wartremover < Formula
+  desc "Flexible Scala code linting tool"
+  homepage "https://github.com/wartremover/wartremover"
+  url "https://github.com/wartremover/wartremover/archive/refs/tags/v3.3.3.tar.gz"
+  sha256 "c067f5b30f49c91a639622dc7f10fb8f5bf8e988a665a185f27ea143beb303d7"
+  license "Apache-2.0"
+  head "https://github.com/wartremover/wartremover.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "acec7256e71f6503a1de7b7c340eaf0e461dfd4c0037fbc8c1e42b45f74916b1"
+  end
+
+  depends_on "sbt" => :build
+  # depends_on "openjdk"
+
+  def install
+    system "sbt", "-sbt-jar", Formula["sbt"].opt_libexec/"bin/sbt-launch.jar", "core/assembly"
+    libexec.install "wartremover-assembly.jar"
+    bin.write_jar_script libexec/"wartremover-assembly.jar", "wartremover"
+  end
+
+  test do
+    (testpath/"foo").write <<~EOS
+      object Foo {
+        def foo() {
+          var msg = "Hello World"
+          println(msg)
+        }
+      }
+    EOS
+    cmd = "#{bin}/wartremover -traverser org.wartremover.warts.Unsafe foo 2>&1"
+    assert_match "var is disabled", shell_output(cmd, 1)
+  end
+end
