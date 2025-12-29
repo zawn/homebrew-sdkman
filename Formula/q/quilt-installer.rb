6bcd06e24918087c4cd2012b6da8199177df4f0e
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:09 +0800

    Sync quilt-installer.rb from core@4cefec9
    RelPath: Formula/q/quilt-installer.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/q/quilt-installer.rb
@@ -0,0 +1,28 @@
+class QuiltInstaller < Formula
+  desc "Installer for Quilt for the vanilla launcher"
+  homepage "https://quiltmc.org/"
+  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.12.1/quilt-installer-0.12.1.jar"
+  sha256 "8b716edc692a2fa1fb78dbc2f432643be1bc6c867e5605f36f691f44257120ca"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c2c3e3aa829bedf8617e2a6159dd32b8f80177f6ad1035e9a5fc51bef91330ab"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "quilt-installer-#{version}.jar"
+    bin.write_jar_script libexec/"quilt-installer-#{version}.jar", "quilt-installer"
+  end
+
+  test do
+    system bin/"quilt-installer", "install", "server", "1.19.2"
+    assert_path_exists testpath/"server/quilt-server-launch.jar"
+  end
+end
