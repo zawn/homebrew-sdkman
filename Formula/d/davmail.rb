487db50316d3bb3cd32a406fdeb43382786b36a3
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:08 +0800

    Sync davmail.rb from core@4cefec9
    RelPath: Formula/d/davmail.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/d/davmail.rb
@@ -0,0 +1,70 @@
+class Davmail < Formula
+  desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
+  homepage "https://davmail.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/davmail/davmail/6.3.0/davmail-6.3.0-3627.zip"
+  version "6.3.0"
+  sha256 "621eb5647b04305e927212c4c81577ad123ba49fdbbd1d8ae06f14ea3acf6b70"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://sourceforge.net/projects/davmail/rss?path=/davmail"
+    regex(%r{url=.*?/davmail[._-]v?(\d+(?:\.\d+)+)(?:-\d+)?\.(?:t|zip)}i)
+  end
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c48497283b8ffca924a12cbaa5227d0653b3c37990540eeb01593cf7fa7b93fa"
+  end
+
+  # depends_on "openjdk"
+
+  uses_from_macos "netcat" => :test
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_jar_script libexec/"davmail.jar", "davmail", "-Djava.awt.headless=true"
+  end
+
+  service do
+    run opt_bin/"davmail"
+    run_type :interval
+    interval 300
+    keep_alive false
+    environment_variables PATH: std_service_path_env
+    log_path File::NULL
+    error_log_path File::NULL
+  end
+
+  test do
+    caldav_port = free_port
+    imap_port = free_port
+    ldap_port = free_port
+    pop_port = free_port
+    smtp_port = free_port
+
+    (testpath/"davmail.properties").write <<~EOS
+      davmail.server=true
+      davmail.mode=auto
+      davmail.url=https://example.com
+
+      davmail.caldavPort=#{caldav_port}
+      davmail.imapPort=#{imap_port}
+      davmail.ldapPort=#{ldap_port}
+      davmail.popPort=#{pop_port}
+      davmail.smtpPort=#{smtp_port}
+    EOS
+
+    fork do
+      exec bin/"davmail", testpath/"davmail.properties"
+    end
+
+    sleep 10
+
+    system "nc", "-z", "localhost", caldav_port
+    system "nc", "-z", "localhost", imap_port
+    system "nc", "-z", "localhost", ldap_port
+    system "nc", "-z", "localhost", pop_port
+    system "nc", "-z", "localhost", smtp_port
+  end
+end
