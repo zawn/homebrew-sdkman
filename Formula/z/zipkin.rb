975da6edd8d3ce554ac4d55fae1193f253644c85
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:00 +0800

    Sync zipkin.rb from core@4cefec9
    RelPath: Formula/z/zipkin.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/z/zipkin.rb
@@ -0,0 +1,39 @@
+class Zipkin < Formula
+  desc "Collect and visualize traces written in Zipkin format"
+  homepage "https://zipkin.io"
+  url "https://search.maven.org/remotecontent?filepath=io/zipkin/zipkin-server/3.5.1/zipkin-server-3.5.1-exec.jar"
+  sha256 "7ac633a6832647e71e50012a5e3e91fbc7054414955261724526030faf271d8d"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=io/zipkin/zipkin-server/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b98f9e0bd540f9675cfb0c9770856b1b8acd8b4d9ee00b9aa03732ea3dafd7bc"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    (libexec/"bin").install "zipkin-server-#{version}-exec.jar"
+    bin.write_jar_script libexec/"bin/zipkin-server-#{version}-exec.jar", "zipkin"
+  end
+
+  service do
+    run opt_bin/"zipkin"
+    keep_alive true
+    log_path var/"log/zipkin.log"
+    error_log_path var/"log/zipkin.log"
+  end
+
+  test do
+    port = free_port
+    ENV["QUERY_PORT"] = port.to_s
+
+    spawn bin/"zipkin"
+    sleep 20
+    assert_match "UP", shell_output("curl -s 127.0.0.1:#{port}/health")
+  end
+end
