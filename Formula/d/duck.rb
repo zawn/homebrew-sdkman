f1a0640fd3605397c7427f2f0da1671a2bf4eee2
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:08 +0800

    Sync duck.rb from core@4cefec9
    RelPath: Formula/d/duck.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/d/duck.rb
@@ -0,0 +1,188 @@
+class Duck < Formula
+  desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
+  homepage "https://duck.sh/"
+  url "https://dist.duck.sh/duck-src-9.1.6.43284.tar.gz"
+  sha256 "e2619e3386877db4f703d90af9b86476230b0404c8bd0726771d937a5728016e"
+  license "GPL-3.0-only"
+  head "https://github.com/iterate-ch/cyberduck.git", branch: "master"
+
+  livecheck do
+    url "https://dist.duck.sh/"
+    regex(/href=.*?duck(?:-src)?[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "22fbb71f82f50e9b7be9ab9bd8560c5d9785868d1d26f54c83a54898801271ce"
+    sha256 cellar: :any, arm64_sonoma:  "3eb0320f9bbe309dfe9042a0f17a3be2cf9370ca2aaa524f252cb97d8af1dccc"
+    sha256 cellar: :any, arm64_ventura: "d8168f1d5e856b9360ee4e5a4c293cc500776795b30c31d00bac0f68373ee6f3"
+    sha256 cellar: :any, sonoma:        "06b08c6e24e60afecb0aabb367350774c2f6409f9cc7afab07875c970cfe978e"
+    sha256 cellar: :any, ventura:       "7e3079ff69e78e88b6f417ce12f7a145a894e946d8858270d70fda8cdef1f791"
+    sha256               arm64_linux:   "2dac6266c8f2f6a7b26037f5cad626000c0b07b94a206ef264bbec1527479a7c"
+    sha256               x86_64_linux:  "9c609ac526a03bb5b0cdbd50e477cb819479d8c95ca571c4f26619ad07d4c3ee"
+  end
+
+  depends_on "ant" => :build
+  depends_on "maven" => :build
+  depends_on "pkgconf" => :build
+  depends_on xcode: ["13.1", :build]
+  # depends_on "openjdk"
+
+  uses_from_macos "libffi", since: :monterey # Uses `FFI_BAD_ARGTYPE`.
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "freetype"
+    depends_on "giflib"
+    depends_on "harfbuzz"
+    depends_on "jpeg-turbo"
+    depends_on "libpng"
+    depends_on "libx11"
+    depends_on "libxext"
+    depends_on "libxi"
+    depends_on "libxrender"
+    depends_on "libxtst"
+    depends_on "little-cms2"
+  end
+
+  conflicts_with "duckscript", because: "both install `duck` binaries"
+
+  resource "jna" do
+    url "https://github.com/java-native-access/jna/archive/refs/tags/5.17.0.tar.gz"
+    sha256 "fe35d85e63ea998ec08f1beaa0162b1e7f233e9e82b8f43ba500a0a824874158"
+  end
+
+  resource "rococoa" do
+    url "https://github.com/iterate-ch/rococoa/archive/refs/tags/0.10.0.tar.gz"
+    sha256 "8ce789a7b27c37ed37dcb6517b76de8eee144bf7269c3c645d791f21c20a3046"
+  end
+
+  resource "JavaNativeFoundation" do
+    url "https://github.com/apple/openjdk/archive/refs/tags/iTunesOpenJDK-1014.0.2.12.1.tar.gz"
+    sha256 "e8556a73ea36c75953078dfc1bafc9960e64593bc01e733bc772d2e6b519fd4a"
+  end
+
+  def install
+    # Consider creating a formula for this if other formulae need the same library
+    resource("jna").stage do
+      os = if OS.mac?
+        inreplace "native/Makefile" do |s|
+          libffi_libdir = if MacOS.version >= :monterey
+            MacOS.sdk_path/"usr/lib"
+          else
+            Formula["libffi"].opt_lib
+          end
+          # Add linker flags for libffi because Makefile call to pkg-config doesn't seem to work properly.
+          s.change_make_var! "LIBS", "-L#{libffi_libdir} -lffi"
+          library_var = s.get_make_var("LIBRARY")
+          # Force shared library to have dylib extension on macOS instead of jnilib
+          s.change_make_var! "LIBRARY", library_var.sub("JNISFX", "LIBSFX")
+        end
+
+        "mac"
+      else
+        OS.kernel_name
+      end
+
+      # Don't include directory with JNA headers in zip archive.  If we don't do this, they will be deleted
+      # and the zip archive has to be extracted to get them. TODO: ask upstream to provide an option to
+      # disable the zip file generation entirely.
+      inreplace "build.xml",
+                "<zipfileset dir=\"build/headers\" prefix=\"build-package-${os.prefix}-${jni.version}/headers\" />",
+                ""
+
+      system "ant", "-Dbuild.os.name=#{os}",
+                    "-Dbuild.os.arch=#{Hardware::CPU.arch}",
+                    "-Ddynlink.native=true",
+                    "-DCC=#{ENV.cc}",
+                    "native-build-package"
+
+      cd "build" do
+        ENV.deparallelize
+        ENV["JAVA_HOME"] = Language::Java.java_home(Formula["openjdk"].version.major.to_s)
+
+        inreplace "build.sh" do |s|
+          # Fix zip error on macOS because libjnidispatch.dylib is not in file list
+          s.gsub! "libjnidispatch.so", "libjnidispatch.so libjnidispatch.dylib" if OS.mac?
+          # Fix relative path in build script, which is designed to be run out extracted zip archive
+          s.gsub! "cd native", "cd ../native"
+        end
+
+        system "sh", "build.sh"
+        buildpath.install shared_library("libjnidispatch")
+      end
+    end
+
+    resource("JavaNativeFoundation").stage do
+      next unless OS.mac?
+
+      cd "apple/JavaNativeFoundation" do
+        xcodebuild "VALID_ARCHS=#{Hardware::CPU.arch}",
+                   "OTHER_CFLAGS=-Wno-strict-prototypes", # Workaround for Xcode 14.3
+                   "-project", "JavaNativeFoundation.xcodeproj"
+        buildpath.install "build/Release/JavaNativeFoundation.framework"
+      end
+    end
+
+    resource("rococoa").stage do
+      next unless OS.mac?
+
+      # Set MACOSX_DEPLOYMENT_TARGET to avoid linker errors when building rococoa.
+      xcconfig = buildpath/"Overrides.xcconfig"
+      xcconfig.write <<~EOS
+        OTHER_LDFLAGS = -headerpad_max_install_names
+        VALID_ARCHS=#{Hardware::CPU.arch}
+        MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      EOS
+      ENV["XCODE_XCCONFIG_FILE"] = xcconfig
+
+      cd "rococoa/rococoa-core" do
+        xcodebuild "VALID_ARCHS=#{Hardware::CPU.arch}", "-project", "rococoa.xcodeproj"
+        buildpath.install shared_library("build/Release/librococoa")
+      end
+    end
+
+    os = if OS.mac?
+      "osx"
+    else
+      OS.kernel_name.downcase
+    end
+
+    revision = version.to_s.rpartition(".").last
+    system "mvn", "-DskipTests", "-Dconfiguration=default", "-Dgit.commitsCount=#{revision}",
+                  "--projects", "cli/#{os}", "--also-make", "verify"
+
+    libdir, bindir = if OS.mac?
+      %w[Contents/Frameworks Contents/MacOS]
+    else
+      %w[lib/app bin]
+    end.map { |dir| libexec/dir }
+
+    if OS.mac?
+      libexec.install Dir["cli/osx/target/duck.bundle/*"]
+
+      # Remove the `*.tbd` files. They're not needed, and they cause codesigning issues.
+      buildpath.glob("JavaNativeFoundation.framework/**/JavaNativeFoundation.tbd").map(&:unlink)
+      rm_r(libdir/"JavaNativeFoundation.framework")
+      libdir.install buildpath/"JavaNativeFoundation.framework"
+
+      rm libdir/shared_library("librococoa")
+      libdir.install buildpath/shared_library("librococoa")
+
+      # Replace runtime with already installed dependency
+      rm_r libexec/"Contents/PlugIns/Runtime.jre"
+      ln_s Formula["openjdk"].libexec/"openjdk.jdk", libexec/"Contents/PlugIns/Runtime.jre"
+    else
+      libexec.install Dir["cli/linux/target/default/duck/*"]
+    end
+
+    rm libdir/shared_library("libjnidispatch")
+    libdir.install buildpath/shared_library("libjnidispatch")
+    bin.install_symlink "#{bindir}/duck" => "duck"
+  end
+
+  test do
+    system bin/"duck", "--download", "https://ftp.gnu.org/gnu/wget/wget-1.19.4.tar.gz", testpath/"test"
+    assert_equal (testpath/"test").sha256, "93fb96b0f48a20ff5be0d9d9d3c4a986b469cb853131f9d5fe4cc9cecbc8b5b5"
+  end
+end
