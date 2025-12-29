3d16c91b68efbca8f3db1dd23ce7f96b39e5c229
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:01 +0800

    Sync scala.rb from core@4cefec9
    RelPath: Formula/s/scala.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/scala.rb
@@ -0,0 +1,55 @@
+class Scala < Formula
+  desc "JVM-based programming language"
+  homepage "https://dotty.epfl.ch/"
+  url "https://github.com/scala/scala3/releases/download/3.7.2/scala3-3.7.2.tar.gz"
+  sha256 "256a8e52657b433afe7b5584beea3c209dc53f8298fa8b2611a2d25f53fe62da"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://www.scala-lang.org/download/"
+    regex(%r{href=.*?download/v?(\d+(?:\.\d+)+)\.html}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "89b9f9de9ead4571ae1a0a34f39b8062763e1b4851cca36a1691690de56eae45"
+  end
+
+  # JDK Compatibility: https://docs.scala-lang.org/overviews/jdk-compatibility/overview.html
+  # depends_on "openjdk"
+
+  conflicts_with "pwntools", because: "both install `common` binaries"
+
+  def install
+    rm Dir["bin/*.bat"]
+
+    libexec.install "lib", "maven2", "VERSION", "libexec"
+    prefix.install "bin"
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+
+    # Set up an IntelliJ compatible symlink farm in 'idea'
+    idea = prefix/"idea"
+    idea.install_symlink libexec/"lib"
+  end
+
+  def caveats
+    <<~EOS
+      To use with IntelliJ, set the Scala home to:
+        #{opt_prefix}/idea
+    EOS
+  end
+
+  test do
+    file = testpath/"Test.scala"
+    file.write <<~SCALA
+      object Test {
+        def main(args: Array[String]): Unit = {
+          println(s"${2 + 2}")
+        }
+      }
+    SCALA
+
+    out = shell_output("#{bin}/scala #{file}").strip
+
+    assert_equal "4", out
+  end
+end
