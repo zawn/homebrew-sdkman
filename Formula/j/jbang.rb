fe26e98b9e4e27a9c407541a1dd7577e983f264b
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:06 +0800

    Sync jbang.rb from core@4cefec9
    RelPath: Formula/j/jbang.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/j/jbang.rb
@@ -0,0 +1,30 @@
+class Jbang < Formula
+  desc "Tool to create, edit and run self-contained source-only Java programs"
+  homepage "https://jbang.dev/"
+  url "https://github.com/jbangdev/jbang/releases/download/v0.128.7/jbang-0.128.7.zip"
+  sha256 "bd2fb359d7f94c5e0c745a21664d777dd8dda5a78bc2daa696868a7f623fd647"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8750c56c067b236ac87ae862ef3bdd1d60bd9d4a7cee2c7cb53aa57b38470ad4"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/jbang", /^abs_jbang_dir=.*/, "abs_jbang_dir=#{libexec}/bin"
+    bin.install_symlink libexec/"bin/jbang"
+  end
+
+  test do
+    system bin/"jbang", "init", "--template=cli", testpath/"hello.java"
+    assert_match "hello made with jbang", (testpath/"hello.java").read
+    assert_match version.to_s, shell_output("#{bin}/jbang --version 2>&1")
+  end
+end
