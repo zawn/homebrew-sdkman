f87cc431523e75b7b621115a39dd6640ecaba65f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:07 +0800

    Sync jdtls.rb from core@4cefec9
    RelPath: Formula/j/jdtls.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/j/jdtls.rb
@@ -0,0 +1,54 @@
+class Jdtls < Formula
+  include Language::Python::Shebang
+
+  desc "Java language specific implementation of the Language Server Protocol"
+  homepage "https://github.com/eclipse-jdtls/eclipse.jdt.ls"
+  url "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.49.0/jdt-language-server-1.49.0-202507311558.tar.gz"
+  version "1.49.0"
+  sha256 "a3f9fb5921f5273d0f8fe4365b363fbad1bdc2e86991db3149b2d76f1265bcd7"
+  license "EPL-2.0"
+  version_scheme 1
+
+  livecheck do
+    url "https://download.eclipse.org/jdtls/milestones/"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a7a9e2f56d30c89c9c2b2d25c9aedb59884f94a5bff265460296e81fe070c4b7"
+  end
+
+  # depends_on "openjdk"
+  depends_on "python@3.13"
+
+  def install
+    libexec.install buildpath.glob("*") - buildpath.glob("config*win*")
+    rewrite_shebang detected_python_shebang, libexec/"bin/jdtls"
+    (bin/"jdtls").write_env_script libexec/"bin/jdtls", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"jdtls", "-configuration", testpath/"config", "-data", testpath/"data") do |stdin, stdout, _e, w|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+      Process.kill("KILL", w.pid)
+    end
+  end
+end
