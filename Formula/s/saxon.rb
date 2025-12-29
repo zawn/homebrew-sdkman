78ed30f6ef7c358bc055b2db8ee7651037604168
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:02 +0800

    Sync saxon.rb from core@4cefec9
    RelPath: Formula/s/saxon.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/saxon.rb
@@ -0,0 +1,57 @@
+class Saxon < Formula
+  desc "XSLT and XQuery processor"
+  homepage "https://github.com/Saxonica/Saxon-HE"
+  url "https://github.com/Saxonica/Saxon-HE/releases/download/SaxonHE-12-8/SaxonHE12-8J.zip"
+  version "12.8"
+  sha256 "2ba851aec7925b882208182c48c936230205d558e335636bbe46626bd8003598"
+  license all_of: ["BSD-3-Clause", "MIT", "MPL-2.0"]
+
+  livecheck do
+    url :stable
+    regex(/^SaxonHE[._-]?v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]&.tr("-", ".")
+    end
+  end
+
+  no_autobump! because: :incompatible_version_format
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "52b46e48018fc8f9837456bf22e740bb5c1372c481465f15198e7c196b1e0017"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*.jar", "doc", "lib", "notices"]
+    bin.write_jar_script libexec/"saxon-he-#{version.major_minor}.jar", "saxon"
+  end
+
+  test do
+    (testpath/"test.xml").write <<~XML
+      <test>It works!</test>
+    XML
+    (testpath/"test.xsl").write <<~XSL
+      <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
+        <xsl:template match="/">
+          <html>
+            <body>
+              <p><xsl:value-of select="test"/></p>
+            </body>
+          </html>
+        </xsl:template>
+      </xsl:stylesheet>
+    XSL
+    assert_equal <<~HTML.chop, shell_output("#{bin}/saxon test.xml test.xsl")
+      <!DOCTYPE HTML>
+      <html>
+         <body>
+            <p>It works!</p>
+         </body>
+      </html>
+    HTML
+  end
+end
