1e91d1d6447fef0fd21de9e535c1654ecb5e2661
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:03 +0800

    Sync fgbio.rb from core@4cefec9
    RelPath: Formula/f/fgbio.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/f/fgbio.rb
@@ -0,0 +1,28 @@
+class Fgbio < Formula
+  desc "Tools for working with genomic and high throughput sequencing data"
+  homepage "https://fulcrumgenomics.github.io/fgbio/"
+  url "https://github.com/fulcrumgenomics/fgbio/releases/download/2.5.21/fgbio-2.5.21.jar"
+  sha256 "55319fdc5df8f38eb5b1c83e34ab1561494b8d8fe59f27e0b5a807a4c70db2dd"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "74519df628d1cfcb37e8b0b897d880fa5c15e7c01ea62bdddc903e6c13853049"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "fgbio-#{version}.jar"
+    bin.write_jar_script libexec/"fgbio-#{version}.jar", "fgbio"
+  end
+
+  test do
+    (testpath/"test.fasta").write <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCT
+      ctgtgtggattaaaaaaagagtgtctgatagcagc
+    EOS
+    cmd = "#{bin}/fgbio HardMaskFasta -i test.fasta -o /dev/stdout"
+    assert_match "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", shell_output(cmd)
+  end
+end
