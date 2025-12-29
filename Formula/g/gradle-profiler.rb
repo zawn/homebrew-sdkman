bcf99bfcc0cd423ffb011358baaefc48c4b761ae
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:40:59 +0800

    Sync gradle-profiler.rb from core@4cefec9
    RelPath: Formula/g/gradle-profiler.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/g/gradle-profiler.rb
@@ -0,0 +1,33 @@
+class GradleProfiler < Formula
+  desc "Profiling and benchmarking tool for Gradle builds"
+  homepage "https://github.com/gradle/gradle-profiler/"
+  url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/0.22.0/gradle-profiler-0.22.0.zip"
+  sha256 "a269f05861d2682c3b98d050330764200bc322024762295713fab167d133283c"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/maven-metadata.xml"
+    regex(%r{<version>\s*v?(\d+(?:\.\d+)+)\s*</version>}i)
+  end
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "4fdec547a78284921b7ccb437e045ec11fa0d0b16cf4959fbd2ddd8081209498"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm(Dir["bin/*.bat"])
+    libexec.install %w[bin lib]
+    env = Language::Java.overridable_java_home_env
+    (bin/"gradle-profiler").write_env_script libexec/"bin/gradle-profiler", env
+  end
+
+  test do
+    (testpath/"settings.gradle").write ""
+    (testpath/"build.gradle").write 'println "Hello"'
+    output = shell_output("#{bin}/gradle-profiler --gradle-version 8.14 --profile chrome-trace")
+    assert_includes output, "* Writing results to"
+  end
+end
