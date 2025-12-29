f2edca1bc08f07cb7cff87f2ace2f08bf3cab7e3
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:07 +0800

    Sync checkstyle.rb from core@4cefec9
    RelPath: Formula/c/checkstyle.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/c/checkstyle.rb
@@ -0,0 +1,33 @@
+class Checkstyle < Formula
+  desc "Check Java source against a coding standard"
+  homepage "https://checkstyle.sourceforge.io/"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.26.1/checkstyle-10.26.1-all.jar"
+  sha256 "e41c24433723ba310a30e41da4f449c105ad47cab2ae9e6be5e06606a647dbca"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2359646775c1f18596721331ef78f36944bc1b8fe98a93d3c25536ba7ec4ad70"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "checkstyle-#{version}-all.jar"
+    bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
+  end
+
+  test do
+    path = testpath/"foo.java"
+    path.write "public class Foo{ }\n"
+
+    output = shell_output("#{bin}/checkstyle -c /sun_checks.xml #{path}", 2)
+    errors = output.lines.select { |line| line.start_with?("[ERROR] #{path}") }
+    assert_match "#{path}:1:17: '{' is not preceded with whitespace.", errors.join(" ")
+    assert_equal errors.size, $CHILD_STATUS.exitstatus
+  end
+end
