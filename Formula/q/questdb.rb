8f3393fa867343cb20d6b40c00d517f685b96f4e
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:09 +0800

    Sync questdb.rb from core@4cefec9
    RelPath: Formula/q/questdb.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/q/questdb.rb
@@ -0,0 +1,57 @@
+class Questdb < Formula
+  desc "Time Series Database"
+  homepage "https://questdb.io"
+  url "https://github.com/questdb/questdb/releases/download/9.0.1/questdb-9.0.1-no-jre-bin.tar.gz"
+  sha256 "9e72107cad87fa38443979afa81a1b90ebbf458731adc5e72d7b6b4d4e6db2a7"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0894694a55beb7c61100f7840a4c1aaa4def4ce8e969b76799d015501371df6c"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm_r("questdb.exe")
+    libexec.install Dir["*"]
+    (bin/"questdb").write_env_script libexec/"questdb.sh", Language::Java.overridable_java_home_env
+    inreplace libexec/"questdb.sh", "/usr/local/var/questdb", var/"questdb"
+  end
+
+  def post_install
+    # Make sure the var/questdb directory exists
+    (var/"questdb").mkpath
+  end
+
+  service do
+    run [opt_bin/"questdb", "start", "-d", var/"questdb", "-n", "-f"]
+    keep_alive successful_exit: false
+    error_log_path var/"log/questdb.log"
+    log_path var/"log/questdb.log"
+    working_dir var/"questdb"
+  end
+
+  test do
+    # questdb.sh uses `ps | grep` to verify server is running, but output is truncated to COLUMNS
+    # See https://github.com/Homebrew/homebrew-core/pull/133887#issuecomment-1679907729
+    ENV.delete "COLUMNS" if OS.linux?
+
+    mkdir_p testpath/"data"
+    begin
+      fork do
+        exec bin/"questdb", "start", "-d", testpath/"data"
+      end
+      sleep 30
+      output = shell_output("curl -Is localhost:9000/index.html")
+      sleep 8
+      assert_match "questDB", output
+    ensure
+      system bin/"questdb", "stop"
+    end
+  end
+end
