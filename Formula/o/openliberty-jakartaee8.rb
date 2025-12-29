c16f00cea9451dcb5353aa9b54a3e3d3f246cd49
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:04 +0800

    Sync openliberty-jakartaee8.rb from core@4cefec9
    RelPath: Formula/o/openliberty-jakartaee8.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/o/openliberty-jakartaee8.rb
@@ -0,0 +1,47 @@
+class OpenlibertyJakartaee8 < Formula
+  desc "Lightweight open framework for Java (Jakarta EE 8)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/25.0.0.7/openliberty-javaee8-25.0.0.7.zip"
+  sha256 "797952870f8f8c90d900aa1002b2973982acc9d8603927e5b599cfa39a25f0b5"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e2db20924d856ab127287ea82a70835f7d6aba4c0f8edd019b8a1384da433027"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm_r(Dir["bin/**/*.bat"])
+
+    libexec.install Dir["*"]
+    (bin/"openliberty-jakartaee8").write_env_script "#{libexec}/bin/server",
+                                                    Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Jakarta EE 8 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-jakartaee8", "start"
+      assert_path_exists testpath/"servers/.pid/defaultServer.pid"
+    ensure
+      system bin/"openliberty-jakartaee8", "stop"
+    end
+
+    refute_path_exists testpath/"servers/.pid/defaultServer.pid"
+    assert_match "<feature>javaee-8.0</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
