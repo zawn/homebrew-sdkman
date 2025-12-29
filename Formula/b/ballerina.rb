83cc9f9008b1442b5115660e18a8f75ebe714bf0
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:10 +0800

    Sync ballerina.rb from core@4cefec9
    RelPath: Formula/b/ballerina.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/b/ballerina.rb
@@ -0,0 +1,47 @@
+class Ballerina < Formula
+  desc "Programming Language for Network Distributed Applications"
+  homepage "https://ballerina.io"
+  url "https://dist.ballerina.io/downloads/2201.12.7/ballerina-2201.12.7-swan-lake.zip"
+  sha256 "2ca6eef7a40c644c5c33d86e20f30b66839688096044347d93f0ca0876a2cd0b"
+  license "Apache-2.0"
+
+  # The Downloads and Installation Options pages don't include any version
+  # information or download links in the HTML. This information is instead
+  # found in page-specific JavaScript files but those use a unique cache-busting
+  # hash in the file name. In this scenario, we would have to fetch the page,
+  # find the JS file name in the HTML, and fetch the JS file to identify the
+  # version. To avoid that setup, we identify the version information from the
+  # release notes URL on the Downloads page (though it's a less ideal check).
+  livecheck do
+    url "https://ballerina.io/downloads/"
+    regex(%r{href=.*?release-notes/[^/]*?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "169e932e18291debaf07a73cd2420fa17fcb6d00327d05e2ee10cfd8ab066381"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    # Remove Windows files
+    rm Dir["bin/*.bat"]
+
+    chmod 0755, "bin/bal"
+
+    bin.install "bin/bal"
+    libexec.install Dir["*"]
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"helloWorld.bal").write <<~EOS
+      import ballerina/io;
+      public function main() {
+        io:println("Hello, World!");
+      }
+    EOS
+    output = shell_output("#{bin}/bal run helloWorld.bal")
+    assert_equal "Hello, World!", output.chomp
+  end
+end
