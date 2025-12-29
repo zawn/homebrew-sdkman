e8fba61dc41562e54979f194374612396b62aa3e
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:04 +0800

    Sync openapi-generator.rb from core@4cefec9
    RelPath: Formula/o/openapi-generator.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/o/openapi-generator.rb
@@ -0,0 +1,70 @@
+class OpenapiGenerator < Formula
+  desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
+  homepage "https://openapi-generator.tech/"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/7.14.0/openapi-generator-cli-7.14.0.jar"
+  sha256 "e03186835022ca02da4aa95e3967b6a3b6d44c2e5f7606e6d5c22466f519c757"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8d1701f1cfd34bac144e5089cb451605434054704079ac172930a4cf864d59c5"
+  end
+
+  head do
+    url "https://github.com/OpenAPITools/openapi-generator.git", branch: "master"
+
+    depends_on "maven" => :build
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    if build.head?
+      system "mvn", "clean", "package", "-Dmaven.javadoc.skip=true"
+      libexec.install "modules/openapi-generator-cli/target/openapi-generator-cli.jar"
+    else
+      libexec.install "openapi-generator-cli-#{version}.jar" => "openapi-generator-cli.jar"
+    end
+
+    bin.write_jar_script libexec/"openapi-generator-cli.jar", "openapi-generator"
+  end
+
+  test do
+    # From the OpenAPI Spec website
+    # https://web.archive.org/web/20230505222426/https://swagger.io/docs/specification/basic-structure/
+    (testpath/"minimal.yaml").write <<~YAML
+      ---
+      openapi: 3.0.3
+      info:
+        version: 0.0.0
+        title: Sample API
+      servers:
+        - url: http://api.example.com/v1
+          description: Optional server description, e.g. Main (production) server
+        - url: http://staging-api.example.com
+          description: Optional server description, e.g. Internal staging server for testing
+      paths:
+        /users:
+          get:
+            summary: Returns a list of users.
+            responses:
+              '200':
+                description: A JSON array of user names
+                content:
+                  application/json:
+                    schema:
+                      type: array
+                      items:
+                        type: string
+    YAML
+    system bin/"openapi-generator", "generate", "-i", "minimal.yaml", "-g", "openapi", "-o", "./"
+    # Python is broken for (at least) Java 20
+    system bin/"openapi-generator", "generate", "-i", "minimal.yaml", "-g", "python", "-o", "./"
+  end
+end
