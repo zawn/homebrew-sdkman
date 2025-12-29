77457caaf64ed75464d6d4c58ebf70277441871e
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync payara.rb from core@4cefec9
    RelPath: Formula/p/payara.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/p/payara.rb
@@ -0,0 +1,58 @@
+class Payara < Formula
+  desc "Java EE application server forked from GlassFish"
+  homepage "https://www.payara.fish"
+  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/6.2025.7/payara-6.2025.7.zip"
+  sha256 "4764ebc75454072358b24c1c8f9a8b306884a7a5a33f315ade1a98ff3fcf301e"
+  license any_of: [
+    "CDDL-1.1",
+    { "GPL-2.0-only" => { with: "Classpath-exception-2.0" } },
+  ]
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "40993a16e69a6cf4adf24218ac8cf3194245659250a20347378f5da66999932d"
+  end
+
+  depends_on :macos # The test fails on Linux.
+  # depends_on "openjdk"
+
+  conflicts_with "glassfish", because: "both install the same scripts"
+
+  def install
+    # Remove Windows scripts
+    rm(Dir["**/*.{bat,exe}"])
+
+    inreplace "bin/asadmin", /AS_INSTALL=.*/,
+                             "AS_INSTALL=#{libexec}/glassfish"
+
+    libexec.install Dir["*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env)
+  end
+
+  def caveats
+    <<~EOS
+      You may want to add the following to your .bash_profile:
+        export GLASSFISH_HOME=#{opt_libexec}/glassfish
+        export PATH=${PATH}:${GLASSFISH_HOME}/bin
+    EOS
+  end
+
+  service do
+    run [opt_libexec/"glassfish/bin/asadmin", "start-domain", "--verbose", "domain1"]
+    keep_alive true
+    working_dir opt_libexec/"glassfish"
+    environment_variables GLASSFISH_HOME: opt_libexec/"glassfish"
+  end
+
+  test do
+    ENV["GLASSFISH_HOME"] = opt_libexec/"glassfish"
+    output = shell_output("#{bin}/asadmin list-domains")
+    assert_match "domain1 not running", output
+    assert_match "Command list-domains executed successfully.", output
+  end
+end
