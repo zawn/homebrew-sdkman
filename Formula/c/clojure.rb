ec823b8d18012cfdcc9ea50c50d63c5d95c008d9
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:07 +0800

    Sync clojure.rb from core@4cefec9
    RelPath: Formula/c/clojure.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/c/clojure.rb
@@ -0,0 +1,36 @@
+class Clojure < Formula
+  desc "Dynamic, general-purpose programming language"
+  homepage "https://clojure.org"
+  url "https://github.com/clojure/brew-install/releases/download/1.12.1.1550/clojure-tools-1.12.1.1550.tar.gz"
+  mirror "https://download.clojure.org/install/clojure-tools-1.12.1.1550.tar.gz"
+  sha256 "906c625679c72e7035875988a46392a1d83d16ee1df599a561a2fd33424b0d3f"
+  license "EPL-1.0"
+  version_scheme 1
+
+  livecheck do
+    url "https://raw.githubusercontent.com/clojure/homebrew-tools/master/Formula/clojure.rb"
+    regex(/url ".*?clojure-tools-v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "4fd0a9a23d271805a3632629f226e436cf9a6532c3504b3b603e2f5156584683"
+  end
+
+  # depends_on "openjdk"
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "./install.sh", prefix
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj", "-e", "nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -e \"(+ 1 1)\"").strip
+    end
+  end
+end
