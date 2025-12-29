a39d69a3d59299c4a8fb2a833d5f7c064e2b0c91
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:04 +0800

    Sync openliberty-microprofile4.rb from core@4cefec9
    RelPath: Formula/o/openliberty-microprofile4.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/o/openliberty-microprofile4.rb
@@ -0,0 +1,47 @@
+class OpenlibertyMicroprofile4 < Formula
+  desc "Lightweight open framework for Java (Micro Profile 4)"
+  homepage "https://openliberty.io"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/25.0.0.7/openliberty-microProfile4-25.0.0.7.zip"
+  sha256 "633054e75fbadfa5f54b6a6ffd0589b89d0ca559064668abe497ed7d0b6bf29b"
+  license "EPL-1.0"
+
+  livecheck do
+    url "https://openliberty.io/api/builds/data"
+    regex(/openliberty[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "59b893621276c4e1b54d46475347ef187948df76412e68997c85ad79cb506d8a"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm_r(Dir["bin/**/*.bat"])
+
+    libexec.install Dir["*"]
+    (bin/"openliberty-microprofile4").write_env_script "#{libexec}/bin/server",
+                                                       Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Open Liberty Micro Profile 4 is:
+        #{opt_libexec}
+    EOS
+  end
+
+  test do
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"openliberty-microprofile4", "start"
+      assert_path_exists testpath/"servers/.pid/defaultServer.pid"
+    ensure
+      system bin/"openliberty-microprofile4", "stop"
+    end
+
+    refute_path_exists testpath/"servers/.pid/defaultServer.pid"
+    assert_match "<feature>microProfile-4.1</feature>", (testpath/"servers/defaultServer/server.xml").read
+  end
+end
