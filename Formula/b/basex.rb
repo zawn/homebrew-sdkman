754bef48632682529a1d5d57139c6173e5d7020f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:09 +0800

    Sync basex.rb from core@4cefec9
    RelPath: Formula/b/basex.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/b/basex.rb
@@ -0,0 +1,36 @@
+class Basex < Formula
+  desc "Light-weight XML database and XPath/XQuery processor"
+  homepage "https://basex.org"
+  url "https://files.basex.org/releases/12.0/BaseX120.zip"
+  version "12.0"
+  sha256 "ffd4de680a768dc906efb17cf89dda4a99285d6c93157bd7c67c578299f9f197"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://basex.org/download/"
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/BaseX[._-]?v?(\d+(?:\.\d+)*)\.zip}i)
+  end
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "05607e52e85895c35964198defc61f665b99d0174f74c28dc9d00b25a6ad4de7"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm Dir["bin/*.bat"]
+    rm_r("repo")
+    rm_r("data")
+    rm_r("etc")
+
+    libexec.install Dir["*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    assert_equal "1\n2\n3\n4\n5\n6\n7\n8\n9\n10", shell_output("#{bin}/basex '1 to 10'")
+  end
+end
