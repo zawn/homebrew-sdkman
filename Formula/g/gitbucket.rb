315f81be26207e6db0a8c35388aee872128a1c01
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:00 +0800

    Sync gitbucket.rb from core@4cefec9
    RelPath: Formula/g/gitbucket.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/g/gitbucket.rb
@@ -0,0 +1,48 @@
+class Gitbucket < Formula
+  desc "Git platform powered by Scala offering"
+  homepage "https://gitbucket.github.io/"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.43.0/gitbucket.war"
+  sha256 "c613573af7fa6ecc9a4ee5b98c8aef4bcdde08f78131d704d0ed9048abab2245"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "94affac815c4043fc76ae0b0fa2030adaad24c6e98409a27ba9084cf89d8a3cf"
+  end
+
+  head do
+    url "https://github.com/gitbucket/gitbucket.git", branch: "master"
+    depends_on "sbt" => :build
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    if build.head?
+      system "sbt", "executable"
+      libexec.install "target/executable/gitbucket.war"
+    else
+      libexec.install "gitbucket.war"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      Note: When using `brew services` the port will be 8080.
+    EOS
+  end
+
+  service do
+    run [Formula["openjdk"].opt_bin/"java", "-Dmail.smtp.starttls.enable=true", "-jar", opt_libexec/"gitbucket.war",
+         "--host=127.0.0.1", "--port=8080"]
+  end
+
+  test do
+    java = Formula["openjdk"].opt_bin/"java"
+    fork do
+      $stdout.reopen(testpath/"output")
+      exec "#{java} -jar #{libexec}/gitbucket.war --port=#{free_port}"
+    end
+    sleep 12
+    File.read("output").exclude?("Exception")
+  end
+end
