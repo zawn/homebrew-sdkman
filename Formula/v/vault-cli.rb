a4ad8e54bbd8052243acb755dfe8dddb938f795b
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:09 +0800

    Sync vault-cli.rb from core@4cefec9
    RelPath: Formula/v/vault-cli.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/v/vault-cli.rb
@@ -0,0 +1,33 @@
+class VaultCli < Formula
+  desc "Subversion-like utility to work with Jackrabbit FileVault"
+  homepage "https://jackrabbit.apache.org/filevault/index.html"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/4.0.0/vault-cli-4.0.0-bin.tar.gz"
+  sha256 "d63b105df96888dc2fdf06b52a2241388c0751e5030111de8a963f6b8bc4233d"
+  license "Apache-2.0"
+  head "https://github.com/apache/jackrabbit-filevault.git", branch: "master"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "1b73373cab8dd418dbaab0e07bc6abe2be62906e43001dab430e482c54a7d80b"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    # Remove windows files
+    rm(Dir["bin/*.bat"])
+
+    libexec.install Dir["*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix)
+  end
+
+  test do
+    # Bad test, but we're limited without a Jackrabbit repo to speak to...
+    system bin/"vlt", "--version"
+  end
+end
