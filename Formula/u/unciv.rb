e03763794a737d13ac72671012dedb92dfcfae03
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:40:59 +0800

    Sync unciv.rb from core@4cefec9
    RelPath: Formula/u/unciv.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/u/unciv.rb
@@ -0,0 +1,29 @@
+class Unciv < Formula
+  desc "Open-source Android/Desktop remake of Civ V"
+  homepage "https://github.com/yairm210/Unciv"
+  url "https://github.com/yairm210/Unciv/releases/download/4.17.11-patch1/Unciv.jar"
+  version "4.17.11-patch1"
+  sha256 "7c3ed61a7dfa934b4e71031093ce0a7394c70cb5c1fd9318eb9381ee80e44ef0"
+  license "MPL-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+(?:[._-]?patch\d*)?)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "11d0444cd9807ec4f4d613fc66d7b876b9dc641574f9979120953e06bd5416f8"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "Unciv.jar"
+    bin.write_jar_script libexec/"Unciv.jar", "unciv"
+  end
+
+  test do
+    # Unciv is a GUI application, so there is no cli functionality to test
+    assert_match version.to_str, shell_output("#{bin}/unciv --version")
+  end
+end
