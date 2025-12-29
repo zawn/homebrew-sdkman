109f630f327beeee9625a8fb5593eea3f00ce810
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:07 +0800

    Sync closure-compiler.rb from core@4cefec9
    RelPath: Formula/c/closure-compiler.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/c/closure-compiler.rb
@@ -0,0 +1,36 @@
+class ClosureCompiler < Formula
+  desc "JavaScript optimizing compiler"
+  homepage "https://developers.google.com/closure/compiler"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20250706/closure-compiler-v20250706.jar"
+  sha256 "c9fdb0e9ad384b563ad272f5864c6b85d2c08983bbca376b514a933dbc93d8ec"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/"
+    regex(/href=.*?v?(\d{8})/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "6447e05c3f2841ad24c258c8f6aa48c18efcd4bfc777892c24c1de74ecc223b6"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_jar_script libexec/"closure-compiler-v#{version}.jar", "closure-compiler"
+  end
+
+  test do
+    (testpath/"test.js").write <<~JS
+      (function(){
+        var t = true;
+        return t;
+      })();
+    JS
+    system bin/"closure-compiler",
+           "--js", testpath/"test.js",
+           "--js_output_file", testpath/"out.js"
+    assert_equal (testpath/"out.js").read.chomp, "(function(){return!0})();"
+  end
+end
