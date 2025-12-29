b4a4095a0e670f670040a9365967e8ba476c9261
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:02 +0800

    Sync ammonite-repl.rb from core@4cefec9
    RelPath: Formula/a/ammonite-repl.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/a/ammonite-repl.rb
@@ -0,0 +1,41 @@
+class AmmoniteRepl < Formula
+  desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
+  homepage "https://ammonite.io/"
+  url "https://github.com/com-lihaoyi/Ammonite/releases/download/3.0.2/3.3-3.0.2"
+  version "3.0.2"
+  sha256 "3353e0f87b164a493ad0d5333505f13a28e77c5c57e628daa3be8f85acfbca44"
+  license "MIT"
+
+  # There can be a gap between when a GitHub release is created and when the
+  # release assets are uploaded, so the `GithubLatest` strategy isn't
+  # sufficient here. This checks GitHub asset URLs on the homepage, as it
+  # doesn't appear to be updated until the release assets are available.
+  livecheck do
+    url :homepage
+    regex(%r{href=.*?/releases/download/v?(\d+(?:\.\d+)+(?:[._-]M\d+)?)/}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "a78817794763be017ec9dc58b4fa4956658265675094f317219b57594e50c7be"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    (libexec/"bin").install Dir["*"].first => "amm"
+    chmod 0755, libexec/"bin/amm"
+    env = Language::Java.overridable_java_home_env
+    env["PATH"] = "$JAVA_HOME/bin:$PATH"
+    (bin/"amm").write_env_script libexec/"bin/amm", env
+  end
+
+  test do
+    (testpath/"testscript.sc").write <<~SCALA
+      #!/usr/bin/env amm
+      @main
+      def fn(): Unit = println("hello world!")
+    SCALA
+    output = shell_output("#{bin}/amm #{testpath}/testscript.sc")
+    assert_equal "hello world!", output.lines.last.chomp
+  end
+end
