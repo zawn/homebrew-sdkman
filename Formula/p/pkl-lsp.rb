00155cbc7cf6ded5a73523b7bf66acf6f84cdde3
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync pkl-lsp.rb from core@4cefec9
    RelPath: Formula/p/pkl-lsp.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/p/pkl-lsp.rb
@@ -0,0 +1,50 @@
+class PklLsp < Formula
+  desc "Language server for Pkl"
+  homepage "https://pkl-lang.org/lsp/current/index.html"
+  url "https://github.com/apple/pkl-lsp/releases/download/0.4.0/pkl-lsp-0.4.0.jar"
+  sha256 "4e2f63416f258f3117d75d6b1c20b718c7c418b974461960fffe1d051e0424ba"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "2c1c5a522fb441d2587911e97756e1fe926ad91ec099fc626b7aee0dc83f0889"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "pkl-lsp-#{version}.jar" => "pkl-lsp.jar"
+    bin.write_jar_script libexec/"pkl-lsp.jar", "pkl-lsp"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pkl-lsp --version")
+
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"pkl-lsp") do |stdin, stdout, _, w|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      stdin.close
+      sleep 1
+      sleep 2 if OS.mac? && Hardware::CPU.intel?
+      Process.kill("TERM", w.pid)
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
