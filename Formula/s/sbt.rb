295ea0a0a00b2c1ede301d803b835de910ff2970
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:01 +0800

    Sync sbt.rb from core@4cefec9
    RelPath: Formula/s/sbt.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/sbt.rb
@@ -0,0 +1,54 @@
+class Sbt < Formula
+  desc "Build tool for Scala projects"
+  homepage "https://www.scala-sbt.org/"
+  url "https://github.com/sbt/sbt/releases/download/v1.11.4/sbt-1.11.4.tgz"
+  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.11.4/sbt-1.11.4.tgz"
+  sha256 "d4b5664264e1d752915f78cc39a49e681df6f61d1abf61cea6966cb221046ab6"
+  license "Apache-2.0"
+
+  # Upstream sometimes creates releases that use a stable tag (e.g., `v1.2.3`)
+  # but are labeled as "pre-release" on GitHub.
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "aea07b96cf849f59b6f26b0867d8e9ee7690bafbacbd76b303ce662050ec7882"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    inreplace "bin/sbt" do |s|
+      s.gsub! 'etc_sbt_opts_file="/etc/sbt/sbtopts"', "etc_sbt_opts_file=\"#{etc}/sbtopts\""
+      s.gsub! "/etc/sbt/sbtopts", "#{etc}/sbtopts"
+    end
+
+    libexec.install "bin"
+    etc.install "conf/sbtopts"
+
+    # Removes:
+    # 1. `sbt.bat` (Windows-only)
+    # 2. `sbtn` (pre-compiled native binary)
+    # 3. `sbtn-universal-apple-darwin` (universal binary)
+    (libexec/"bin").glob("sbt{.bat,n-x86_64*,n-aarch64*,n-universal-apple-darwin}").map(&:unlink)
+    (bin/"sbt").write_env_script libexec/"bin/sbt", Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      You can use $SBT_OPTS to pass additional JVM options to sbt.
+      Project specific options should be placed in .sbtopts in the root of your project.
+      Global settings should be placed in #{etc}/sbtopts
+
+      The installation does not include `sbtn`.
+    EOS
+  end
+
+  test do
+    ENV.append "_JAVA_OPTIONS", "-Dsbt.log.noformat=true"
+    system bin/"sbt", "--sbt-create", "about"
+    assert_match version.to_s, shell_output("#{bin}/sbt sbtVersion --allow-empty")
+  end
+end
