cde8318c060d229c5d588f59cdb6723e188fb6be
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:01 +0800

    Sync sonar-scanner.rb from core@4cefec9
    RelPath: Formula/s/sonar-scanner.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/sonar-scanner.rb
@@ -0,0 +1,29 @@
+class SonarScanner < Formula
+  desc "Launcher to analyze a project with SonarQube"
+  homepage "https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/"
+  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-7.2.0.5079.zip"
+  sha256 "30d80b37c8f4be1254b6c43ec8c64291798d97a710c28475958b5e2f10809fe1"
+  license "LGPL-3.0-or-later"
+  head "https://github.com/SonarSource/sonar-scanner-cli.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "461384188d20091347a182bc82c48f2b1b655cb1e9c734bff02d17c9fb7c701a"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    rm_r(Dir["bin/*.bat"])
+    libexec.install Dir["*"]
+    bin.install libexec/"bin/sonar-scanner"
+    etc.install libexec/"conf/sonar-scanner.properties"
+    ln_s etc/"sonar-scanner.properties", libexec/"conf/sonar-scanner.properties"
+    bin.env_script_all_files libexec/"bin/",
+                              SONAR_SCANNER_HOME: libexec,
+                              JAVA_HOME:          Language::Java.overridable_java_home_env[:JAVA_HOME]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sonar-scanner --version")
+  end
+end
