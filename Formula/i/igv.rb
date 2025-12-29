80b0ef5c7a27965514e84c3606a14f0b45f8025c
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:40:59 +0800

    Sync igv.rb from core@4cefec9
    RelPath: Formula/i/igv.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/i/igv.rb
@@ -0,0 +1,35 @@
+class Igv < Formula
+  desc "Interactive Genomics Viewer"
+  homepage "https://igv.org/doc/desktop/"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.19/IGV_2.19.5.zip"
+  sha256 "0b5f542b3639552d4f01fa3dda78ffffc77fa137fd72fe8bcf583ed4b51b03f0"
+  license "MIT"
+
+  livecheck do
+    url "https://igv.org/doc/desktop/DownloadPage/"
+    regex(/href=.*?IGV[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7f637e64cc7e117ea087c98ff6ad701d992d75c057e208e4bf60655da0ba3ad8"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    inreplace ["igv.sh", "igvtools"], /^prefix=.*/, "prefix=#{libexec}"
+    bin.install "igv.sh" => "igv"
+    bin.install "igvtools"
+    libexec.install "igv.args", "lib"
+    bin.env_script_all_files libexec, Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/igvtools")
+    assert_match "org/broad/igv/ui/IGV.class", shell_output("#{Formula["openjdk"].bin}/jar tf #{libexec}/lib/igv.jar")
+
+    ENV.append "_JAVA_OPTIONS", "-Duser.home=#{testpath}"
+    (testpath/"script").write "exit"
+    assert_match "Using system JDK.", shell_output("#{bin}/igv -b script")
+  end
+end
