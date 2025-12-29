5738ffdb878933254997f83cede50bd92ab83198
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:04 +0800

    Sync orc-tools.rb from core@4cefec9
    RelPath: Formula/o/orc-tools.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/o/orc-tools.rb
@@ -0,0 +1,27 @@
+class OrcTools < Formula
+  desc "ORC java command-line tools and utilities"
+  homepage "https://orc.apache.org/"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/2.2.0/orc-tools-2.2.0-uber.jar"
+  sha256 "c925c93360acd270f38a58a7c0fc4a28151e1e4ff836e9c987aa54eb5e0a6a51"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ab98809eb8388d714d74e72bc1b2e1cddd780d962a8126ac8e43790371f94ec6"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "orc-tools-#{version}-uber.jar"
+    bin.write_jar_script libexec/"orc-tools-#{version}-uber.jar", "orc-tools"
+  end
+
+  test do
+    system bin/"orc-tools", "meta", "-h"
+  end
+end
