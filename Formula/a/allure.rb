b4a6f0c94a28c0c27373a4ea73654fd37e63c9d3
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:03 +0800

    Sync allure.rb from core@4cefec9
    RelPath: Formula/a/allure.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/a/allure.rb
@@ -0,0 +1,56 @@
+class Allure < Formula
+  desc "Flexible lightweight test report tool"
+  homepage "https://github.com/allure-framework/allure2"
+  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.34.1/allure-commandline-2.34.1.zip"
+  sha256 "6eb2e037604744d4ba1f9cad3676cf286974aca48f7839d9b2f7688e1efd79f2"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=io/qameta/allure/allure-commandline/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8671464d1f75b3438591d2668848c61d47272eda34f851ada9b3e10e89d550ac"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    # Remove all windows files
+    rm(Dir["bin/*.bat"])
+
+    libexec.install Dir["*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    (testpath/"allure-results/allure-result.json").write <<~JSON
+      {
+        "uuid": "allure",
+        "name": "testReportGeneration",
+        "fullName": "org.homebrew.AllureFormula.testReportGeneration",
+        "status": "passed",
+        "stage": "finished",
+        "start": 1494857300486,
+        "stop": 1494857300492,
+        "labels": [
+          {
+            "name": "package",
+            "value": "org.homebrew"
+          },
+          {
+            "name": "testClass",
+            "value": "AllureFormula"
+          },
+          {
+            "name": "testMethod",
+            "value": "testReportGeneration"
+          }
+        ]
+      }
+    JSON
+    system bin/"allure", "generate", "#{testpath}/allure-results", "-o", "#{testpath}/allure-report"
+  end
+end
