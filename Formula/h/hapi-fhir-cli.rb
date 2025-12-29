ea36ee775ee45c496be05e99cf507d70a782b98f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:04 +0800

    Sync hapi-fhir-cli.rb from core@4cefec9
    RelPath: Formula/h/hapi-fhir-cli.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/h/hapi-fhir-cli.rb
@@ -0,0 +1,41 @@
+class HapiFhirCli < Formula
+  desc "Command-line interface for the HAPI FHIR library"
+  homepage "https://hapifhir.io/"
+  url "https://github.com/hapifhir/hapi-fhir/releases/download/v8.2.1/hapi-fhir-8.2.1-cli.zip"
+  sha256 "f296900346e182a10093367f78126317ef9f3b17615dc92618f1616170f18a4e"
+  license "Apache-2.0"
+
+  # The "latest" release on GitHub is sometimes for an older major/minor, so we
+  # can't rely on it being the newest version. However, the formula's `stable`
+  # URL is a release asset, so it's necessary to check multiple releases to
+  # identify the highest version.
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7d9e434c853a26a53e93038dc93c8e4c420ea2acf7599ebce14580910f1288c1"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    inreplace "hapi-fhir-cli", /SCRIPTDIR=(.*)/, "SCRIPTDIR=#{libexec}"
+    libexec.install "hapi-fhir-cli.jar"
+    bin.install "hapi-fhir-cli"
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  test do
+    resource "homebrew-test_resource" do
+      url "https://github.com/hapifhir/hapi-fhir/raw/v5.4.0/hapi-fhir-structures-dstu3/src/test/resources/specimen-example.json"
+      sha256 "4eacf47eccec800ffd2ca23b704c70d71bc840aeb755912ffb8596562a0a0f5e"
+    end
+
+    testpath.install resource("homebrew-test_resource")
+    system bin/"hapi-fhir-cli", "validate", "--file", "specimen-example.json",
+           "--fhir-version", "dstu3"
+  end
+end
