48eca7a0a0d9b1ba015e7c97f399ac06c7b2cdb8
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync plantuml.rb from core@4cefec9
    RelPath: Formula/p/plantuml.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/p/plantuml.rb
@@ -0,0 +1,37 @@
+class Plantuml < Formula
+  desc "Draw UML diagrams"
+  homepage "https://plantuml.com/"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2025.4/plantuml-1.2025.4.jar"
+  sha256 "26518e14a3a04100cd76c0d96cab2d1171f36152215edd9790a28d20268200c1"
+  license "GPL-3.0-or-later"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e353eefa552f5bde5cf35cb8c94f914839fc17fcb77e177ce1652ab54ad1bac0"
+  end
+
+  depends_on "graphviz"
+  # depends_on "openjdk"
+
+  def install
+    jar = "plantuml.jar"
+    libexec.install "plantuml-#{version}.jar" => jar
+    (bin/"plantuml").write <<~EOS
+      #!/bin/bash
+      if [[ "$*" != *"-gui"* ]]; then
+        VMARGS="-Djava.awt.headless=true"
+      fi
+      GRAPHVIZ_DOT="#{Formula["graphviz"].opt_bin}/dot" exec "#{Formula["openjdk"].opt_bin}/java" $VMARGS -jar #{libexec}/#{jar} "$@"
+    EOS
+    chmod 0755, bin/"plantuml"
+  end
+
+  test do
+    system bin/"plantuml", "-testdot"
+  end
+end
