b731d8cbea0172ae9eaa40c9d3f13e55898f104f
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:01 +0800

    Sync surelog.rb from core@4cefec9
    RelPath: Formula/s/surelog.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/surelog.rb
@@ -0,0 +1,116 @@
+class Surelog < Formula
+  desc "SystemVerilog Pre-processor, parser, elaborator, UHDM compiler"
+  homepage "https://github.com/chipsalliance/Surelog"
+  url "https://github.com/chipsalliance/Surelog/archive/refs/tags/v1.84.tar.gz"
+  sha256 "ddcbc0d943ee52f2487b7a064c57a8239d525efd9a45b1f3e3e4a96a56cb3377"
+  license "Apache-2.0"
+  revision 2
+  head "https://github.com/chipsalliance/Surelog.git", branch: "master"
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "727db7b3c19efd328fef24f1eae4d5790e37d6c47a570edcd87a63042486d076"
+    sha256 cellar: :any,                 arm64_sonoma:  "3743df7ef2d3f379e55bc9d8e77fc9de3c9716a76bcb055a86ba5c52e027559c"
+    sha256 cellar: :any,                 arm64_ventura: "d489c736ba9d3ae719c3302d27e62a29a7072d28f5f42e41ed5218948e88436e"
+    sha256 cellar: :any,                 sonoma:        "4ef650973f4b852bf578c0a2df59948e6754b637eb1bbc75e65bfd6f4942b9d2"
+    sha256 cellar: :any,                 ventura:       "b1c5876a1bd6548646d385182550baf7e9a41f64eab6c7cdaf4d6b23075f4850"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6c6afd3cb1e3a200aa037ea9ef4a09e7a136fe30b963ca1d91242dc44332f535"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecfa15659fbee451ba2f7e6649fb7796707edc5de841ce124b031a6162113005"
+  end
+
+  depends_on "antlr" => :build
+  depends_on "cmake" => :build
+  depends_on "nlohmann-json" => :build
+  # depends_on "openjdk" => :build
+  depends_on "python@3.13" => :build
+  depends_on "pkgconf" => :test
+  depends_on "antlr4-cpp-runtime"
+  depends_on "capnp"
+  depends_on "uhdm"
+
+  uses_from_macos "zlib"
+
+  conflicts_with "open-babel", because: "both install `roundtrip` binaries"
+
+  def install
+    antlr = Formula["antlr"]
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DANTLR_JAR_LOCATION=#{antlr.opt_prefix}/antlr-#{antlr.version}-complete.jar",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DPython3_EXECUTABLE=#{which("python3.13")}",
+                    "-DSURELOG_BUILD_TESTS=OFF",
+                    "-DSURELOG_USE_HOST_ALL=ON",
+                    "-DSURELOG_WITH_ZLIB=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # ensure linking is ok
+    system bin/"surelog", "--version"
+
+    # ensure library is ok
+    (testpath/"test.cpp").write <<~CPP
+      #include <Surelog/API/Surelog.h>
+      #include <Surelog/CommandLine/CommandLineParser.h>
+      #include <Surelog/Common/FileSystem.h>
+      #include <Surelog/Design/Design.h>
+      #include <Surelog/Design/ModuleInstance.h>
+      #include <Surelog/ErrorReporting/ErrorContainer.h>
+      #include <Surelog/SourceCompile/SymbolTable.h>
+      #include <functional>
+      #include <iostream>
+      #include <uhdm/uhdm.h>
+      int main(int argc, const char** argv) {
+        uint32_t code = 0;
+        SURELOG::SymbolTable* symbolTable = new SURELOG::SymbolTable();
+        SURELOG::ErrorContainer* errors = new SURELOG::ErrorContainer(symbolTable);
+        SURELOG::CommandLineParser* clp =
+            new SURELOG::CommandLineParser(errors, symbolTable, false, false);
+        clp->noPython();
+        bool success = clp->parseCommandLine(argc, argv);
+        errors->printMessages(clp->muteStdout());
+        SURELOG::Design* the_design = nullptr;
+        SURELOG::scompiler* compiler = nullptr;
+        if (success && (!clp->help())) {
+          compiler = SURELOG::start_compiler(clp);
+          the_design = SURELOG::get_design(compiler);
+          auto stats = errors->getErrorStats();
+          code = (!success) | stats.nbFatal | stats.nbSyntax | stats.nbError;
+        }
+        if (the_design) {
+          for (auto& top : the_design->getTopLevelModuleInstances()) {
+            std::function<void(SURELOG::ModuleInstance*)> inst_visit =
+              [&inst_visit](SURELOG::ModuleInstance* inst) {
+                SURELOG::FileSystem* const fileSystem =
+                  SURELOG::FileSystem::getInstance();
+                  std::cout << "Inst: " << inst->getFullPathName() << std::endl;
+                  std::cout << "File: " << fileSystem->toPath(inst->getFileId())
+                    << std::endl;
+              for (uint32_t i = 0; i < inst->getNbChildren(); i++) {
+                inst_visit(inst->getChildren(i));
+              }
+            };
+            inst_visit(top);
+          }
+        }
+        if (success && (!clp->help())) {
+          SURELOG::shutdown_compiler(compiler);
+        }
+        delete clp;
+        delete symbolTable;
+        delete errors;
+        return code;
+      }
+    CPP
+
+    flags = shell_output("pkgconf --cflags --libs Surelog").chomp.split
+    system ENV.cxx, testpath/"test.cpp", "-o", "test",
+                    "-L#{Formula["antlr4-cpp-runtime"].opt_prefix}/lib",
+                    "-fPIC", "-std=c++17", *flags
+    system testpath/"test"
+  end
+end
