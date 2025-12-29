a6d8c32448515149cb8d01260f26d64c9aeeb17d
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:02 +0800

    Sync spotbugs.rb from core@4cefec9
    RelPath: Formula/s/spotbugs.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/s/spotbugs.rb
@@ -0,0 +1,57 @@
+class Spotbugs < Formula
+  desc "Tool for Java static analysis (FindBugs's successor)"
+  homepage "https://spotbugs.github.io/"
+  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.9.3/spotbugs-4.9.3.tgz"
+  sha256 "d464d56050cf1dbda032e9482e1188f7cd7b7646eaff79c2e6cbe4d6822f4d9f"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/"
+    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "04bc55a24780bb678bad33c70d61444aa1884a76b2f6ea79ee6140c9ecd92c8f"
+  end
+
+  head do
+    url "https://github.com/spotbugs/spotbugs.git", branch: "master"
+
+    depends_on "gradle" => :build
+  end
+
+  # depends_on "openjdk"
+
+  conflicts_with "fb-client", because: "both install a `fb` binary"
+
+  def install
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
+    if build.head?
+      system "gradle", "build"
+      system "gradle", "installDist"
+      libexec.install Dir["spotbugs/build/install/spotbugs/*"]
+    else
+      libexec.install Dir["*"]
+      chmod 0755, "#{libexec}/bin/spotbugs"
+    end
+    (bin/"spotbugs").write_env_script "#{libexec}/bin/spotbugs", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"HelloWorld.java").write <<~JAVA
+      public class HelloWorld {
+        private double[] myList;
+        public static void main(String[] args) {
+          System.out.println("Hello World");
+        }
+        public double[] getList() {
+          return myList;
+        }
+      }
+    JAVA
+    system Formula["openjdk"].bin/"javac", "HelloWorld.java"
+    system Formula["openjdk"].bin/"jar", "cvfe", "HelloWorld.jar", "HelloWorld", "HelloWorld.class"
+    output = shell_output("#{bin}/spotbugs -textui HelloWorld.jar")
+    assert_match(/M V EI.*\nM C UwF.*\n/, output)
+  end
+end
