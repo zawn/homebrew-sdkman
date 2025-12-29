eb4ad76f1e5be6cdc5a48b7f406064a9b4e75bab
zhangzhenli <zhangzhenli@live.com>
2025-12-29 15:41:11 +0800

    Sync wiremock-standalone.rb from core@4cefec9
    RelPath: Formula/w/wiremock-standalone.rb
    Original commit: Merge pull request #232294 from Homebrew/bump-fluent-bit-4.0.6

---
+++ Formula/w/wiremock-standalone.rb
@@ -0,0 +1,40 @@
+class WiremockStandalone < Formula
+  desc "Simulator for HTTP-based APIs"
+  homepage "https://wiremock.org/docs/running-standalone/"
+  url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/3.13.1/wiremock-standalone-3.13.1.jar"
+  sha256 "bdf4c705e7fd61c778e59a19f75396eac4520efeabfac97643a53979bd4d5716"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "ccd5f98ad31ec4796d4b0d51073ead937cefb1e638e08b602f7332708ec8db31"
+  end
+
+  # depends_on "openjdk"
+
+  def install
+    libexec.install "wiremock-standalone-#{version}.jar"
+    bin.write_jar_script libexec/"wiremock-standalone-#{version}.jar", "wiremock"
+  end
+
+  test do
+    port = free_port
+
+    wiremock = fork do
+      exec "#{bin}/wiremock", "-port", port.to_s
+    end
+
+    loop do
+      Utils.popen_read("curl", "-s", "http://localhost:#{port}/__admin/", "-X", "GET")
+      break if $CHILD_STATUS.exitstatus.zero?
+    end
+
+    system "curl", "-s", "http://localhost:#{port}/__admin/shutdown", "-X", "POST"
+
+    Process.wait(wiremock)
+  end
+end
